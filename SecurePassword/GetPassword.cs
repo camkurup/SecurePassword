c6a35bc30eff70a74e7e99e785ca5945e71f8b84
camkurup <camkurup@hotmail.com>
2023-03-07 12:19:22 +0100

    Skellettet er bygget op. Nu sjal der tilføjes noget kode

---
+++ SecurePassword/GetPassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePassword
+{
+    internal class GetPassword
+    {
+        public string GetSalt()
+        {
+            return "";
+        }
+
+        public string GetHash()
+        {
+            return "";
+        }
+
+        public string AddSaltToPassword(string salt, string password)
+        {
+            return salt + password;
+        }
+    }
+}
